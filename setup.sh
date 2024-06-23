903901866d74d0f676e4defac109af78a5e846ca
NaveenKumar075 <naveenkumarforjobs@gmail.com>
2024-06-23 14:38:56 +0530

    Feat: added setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Update package index
+sudo apt-get update
+
+# Install system dependencies
+sudo apt-get install -y libsqlite3-dev
+
+# Install Python packages from requirements.txt
+pip install -r requirements.txt
