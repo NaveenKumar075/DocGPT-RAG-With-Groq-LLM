c9c72d44adcbf639b2a4dcc8c33aa4464d39a420
Naveen Kumar <104119173+NaveenKumar075@users.noreply.github.com>
2024-06-23 23:52:58 +0530

    Feat: Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-path = "/mount/src/DocGPT-RAG-With-Groq-LLM/db"
+path = "DocGPT-RAG-With-Groq-LLM/db"
 sys.path.append(path)
 
 import os
