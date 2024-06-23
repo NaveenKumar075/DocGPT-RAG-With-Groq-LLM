36a35f090090d9717b765a6bc60494443dd437a3
NaveenKumar075 <naveenkumarforjobs@gmail.com>
2024-06-23 14:41:25 +0530

    Feat: updated on setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,8 @@ sudo apt-get update
 # Install system dependencies
 sudo apt-get install -y libsqlite3-dev
 
+# Install wheel package
+pip install wheel
+
 # Install Python packages from requirements.txt
 pip install -r requirements.txt
