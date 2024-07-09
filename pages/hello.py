041c06cda4ab87ce99eb6309bb56b613a86ef85d
Naveen Kumar <104119173+NaveenKumar075@users.noreply.github.com>
2024-07-10 00:20:47 +0530

    Feat: Update hello.py

--- pages/hello.py
+++ pages/hello.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-# st.set_page_config(
-#     page_title="Hello",
-#     page_icon="👋",
-# )
+st.set_page_config(
+    page_title="About Us",
+    page_icon="👋",
+)
 
 st.write("# Welcome to DocGPT! 👋")
 
